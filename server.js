33202bec976f2e4dd08bd699b0e1297d2027dda4
ngaharon <ngairaharon1@gmail.com>
2024-07-16 15:05:04 +0300

    server configuration

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const config = require('./_config');
 
 // Define routes
 let index = require('./routes/index');
@@ -11,43 +12,45 @@ let image = require('./routes/image');
 const app = express();
 
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
-let dbName = 'darkroom';
-mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
-    if (err) console.log(err)
+
+const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
+    if (err) {
+        console.log(err)
+    }else{
+        console.log(`Connected to Database: ${MONGODB_URI}`)
+    }
 });
 
 // test if the database has connected successfully
-let db = mongoose.connection;
-db.once('open', ()=>{
-    console.log('Database connected successfully')
-})
+// let db = mongoose.connection;
+// db.once('open', ()=>{
+//     console.log('Database connected successfully')
+// })
 
-// Initializing the app
-// const app = express();
 
-// View Engine setup (assuming you are using EJS based on your setup)
+
+
+// View Engine
 app.set('view engine', 'ejs');
 
-// Serve static files from the 'public' directory
+// Set up the public folder;
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Parse JSON bodies for API requests
-app.use(express.json());
+// body parser middleware
+app.use(express.json())
 
-// Define routes
+
 app.use('/', index);
 app.use('/image', image);
 
-// Start the server only if not in test mode
-// if (process.env.NODE_ENV !== 'test') {
-//     const PORT = process.env.PORT || 5000;
-//     app.listen(PORT, () => {
-//         console.log(Server is running on http://localhost:${PORT});
-//     });
-// }
 
+
+ 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
+
+
+module.exports = app;
